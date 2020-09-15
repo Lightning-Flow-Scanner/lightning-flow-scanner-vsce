87472c2abf437ec27c7a06dcec25a77a368dd865
Ruben Halman <ruben.halman@pwc.com>
2020-09-15 13:42:43 +0200

    fix rename

--- src/main/commands/RenameFlowCommand.ts
+++ src/main/commands/RenameFlowCommand.ts
@@ -3,6 +3,7 @@ import { SelectAFlow } from "../libs/SelectAFlow";
 import { RenameFlow } from "../libs/RenameFlow";
 import { BaseCommand } from "./BaseCommand";
 import {SaveFlow} from "../libs/SaveFlow";
+import Flow = require("../Models/Flow");
 
 export class RenameFlowCommand extends BaseCommand{
 
@@ -13,8 +14,8 @@ export class RenameFlowCommand extends BaseCommand{
 
   public async execute() {
     const workspacePath = this.getRootPath();
-    const selectedFlow = await new SelectAFlow('Select a Flow to rename:', false).execute(workspacePath);
-    const renamedFlow = await new SaveFlow(workspacePath).execute(selectedFlow);
+    const selectedFlow: Flow = await new SelectAFlow('Select a Flow to rename:').execute(workspacePath);
+    const renamedFlow: Flow = await new SaveFlow(workspacePath).execute(new RenameFlow().execute(selectedFlow));
   }
 
 }
