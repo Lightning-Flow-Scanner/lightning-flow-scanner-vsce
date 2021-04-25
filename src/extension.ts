88a1cb67cae8528d44543ffa33b9cc5441f2d68c
Ruben Halman <halmanrf@gmail.com>
2021-04-25 04:34:12 +0200

    rm unused feats

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,11 @@
 import * as vscode from "vscode";
 
-import { RenameFlowCommand } from "./main/commands/RenameFlowCommand";
-import { MergeFlowsCommand } from "./main/commands/MergeFlowsCommand";
 import { CleanFlowCommand } from "./main/commands/CleanFlowCommand";
 
 export function activate(context: vscode.ExtensionContext) {
-  // let renameFlowCommand = new RenameFlowCommand(context);
-  // let mergeFlowsCommand = new MergeFlowsCommand(context);
   let cleanFlowCommand = new CleanFlowCommand(context);
 
   context.subscriptions.concat([
-    // vscode.commands.registerCommand('flowcontrol.rename', () => renameFlowCommand.execute()),
-    // vscode.commands.registerCommand('flowcontrol.merge', () => mergeFlowsCommand.execute()),
     vscode.commands.registerCommand('flowcontrol.clean', () => cleanFlowCommand.execute())
   ]);
 }
