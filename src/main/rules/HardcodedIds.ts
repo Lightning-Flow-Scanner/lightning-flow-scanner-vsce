c7ed3040f29099a0a6adc539f22d888543be12cf
Ruben <halmanrf@gmail.com>
2021-05-23 23:46:45 +0200

    support references to custom objects

--- src/main/rules/HardcodedIds.ts
+++ src/main/rules/HardcodedIds.ts
@@ -8,7 +8,11 @@ export class HardcodedIds{
             return prefix['Key Prefix'];
         });
         const nodesWithHardcodedIds = [];
-
+        let customPrefixes = new Array(100);
+        for(let i = 0; i < customPrefixes.length; i++){
+            let prefix = ("" + i).length === 1 ? 'a0' + String(i) :'a' + String(i) ;
+            prefixes.push(prefix);
+        }
         for(const prefix of prefixes){
             const match18charIds: RegExp = new RegExp('\\b'+ prefix + '\\w{15}\\b');
             const match15charIds: RegExp = new RegExp('\\b'+ prefix + '\\w{12}\\b');
