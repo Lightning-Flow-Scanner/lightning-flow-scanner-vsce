ba68e16417a0fb844b56ad1c40c0774e2831de8c
Ruben <halmanrf@gmail.com>
2021-07-07 00:15:57 +0200

    fix sidebar

--- webviews/components/Sidebar.svelte
+++ webviews/components/Sidebar.svelte
@@ -1,15 +1,15 @@
 {#if show}
     <nav transition:fly={{x: 350, opacity: 1}}>
         <ol>
-            <input type="checkbox" id="select-all" checked={selectedRules.size === ruleNames.length}
+            <input type="checkbox" id="select-all" checked={allRules.length === selectedRules.size}
                    on:change={onSelectAll}>
             <label for="select-all"><strong>Select all</strong></label>
-            {#each ruleNames as rule}
+            {#each allRules as rule}
                 <li>
-                    <input type="checkbox" id={rule} value={rule} checked={selectedRules.has(rule)}>
-                    <label for={rule}>{rules.find(ruleWT => ruleWT.name === rule).label}</label>
+                    <input type="checkbox" id={rule.name} value={rule.name} checked={selectedRules.has(rule.name)} on:change={onCheckRule}>
+                    <label for={rule}>{rule.label}</label>
                     <Tooltip
-                            title={rules.find(ruleWT => ruleWT.name === rule).text}
+                            title={rule.text}
                     >
                         <a class="tooltip" href="/">?</a>
                     </Tooltip>
@@ -19,24 +19,32 @@
     </nav>
 {/if}
 
-
 <script>
     import {fly} from 'svelte/transition';
     import Tooltip from "./Tooltip.svelte";
     import * as core from 'lightningflowscan-core/out';
 
-    let rules = core.getRuleDefinitions();
-    export let selectedRules = new Set(rules.map(rule => rule.name));
-    let ruleNames = rules.map(rule => rule.name);
-
+    let allRules = core.getRuleDefinitions();
+    const ruleNames = allRules.map(rule => rule.name);
+    export let selectedRules = new Set(ruleNames);
     export let show = true;
-    function onSelectAll(event) {
-        // if (event.target.checked) {
-        //     selectedRules = new Set(ruleNames);
-        // } else {
-        //     selectedRules.clear();
-        // }
-        // selectedRules = selectedRules;
+
+    function onCheckRule(event) {
+        if (event.target.checked) {
+            selectedRules.add(event.target.value);
+
+        } else {
+            selectedRules.delete(event.target.value);
+        }
+        selectedRules = selectedRules;
     }
 
+    function onSelectAll(event) {
+        if (event.target.checked) {
+            selectedRules = new Set(allRules.map(rule => rule.name));
+        } else {
+            selectedRules.clear();
+        }
+        selectedRules = selectedRules;
+    }
 </script>
