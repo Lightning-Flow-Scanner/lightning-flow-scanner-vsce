5895245fa1916bb26bdea0444c43a7c1c21c56d3
Ruben <halmanrf@gmail.com>
2021-10-10 20:55:34 +0200

    show flow coverage

--- src/main/commands/ScanFlowsCommand.ts
+++ src/main/commands/ScanFlowsCommand.ts
@@ -2,15 +2,11 @@ import {BaseCommand} from "./BaseCommand";
 import * as vscode from "vscode";
 import {SelectFlows} from "../libs/SelectFlows";
 import {ParseFlows} from "../libs/ParseFlows";
-import {GetOrgInfo} from "../libs/GetOrgInfo";
 import {LintFlowsReport} from "../panels/LintFlowsReport";
 import * as core from 'lightning-flow-scanner-core/out';
 import {Flow} from "lightning-flow-scanner-core/out/main/models/Flow";
 import {ScanResult} from "lightning-flow-scanner-core/out/main/models/ScanResult";
-import {GetFlowCoverage} from "../libs/GetFlowCoverage";
-import {GetFlowDefinitionViews} from "../libs/GetFlowDefinitionViews";
-
-
+import {FindFlowCoverage} from "../libs/FindFlowCoverage";
 
 export class ScanFlowsCommand extends BaseCommand {
 
@@ -24,22 +20,7 @@ export class ScanFlowsCommand extends BaseCommand {
     if (selectedUris.length > 0) {
       const flows: Flow[] = await new ParseFlows().execute(selectedUris);
       const results: ScanResult[] = core.scan(flows);
-
-      // todo new class
-      const orgInfo = await new GetOrgInfo().getOrgInfo();
-      if(orgInfo.result && orgInfo.result.username){
-        const flowCoverage = await new GetFlowCoverage().getFlowCoverage(orgInfo.result.username);
-        const flowDefinitions = await new GetFlowDefinitionViews().getFlowDefinitionViews(orgInfo.result.username);
-        for (let scanResult of results){
-          const matchingFlowDefinition = flowDefinitions.result.records.find((record) => scanResult.flow.label[0] === record.Label);
-          scanResult['matchingFlowDefinition'] = matchingFlowDefinition;
-          const matchingFlowCoverage = flowCoverage.result.records.find(record => matchingFlowDefinition.ActiveVersionId === record.FlowVersionId);
-          if(matchingFlowCoverage){
-              scanResult['coverage'] = matchingFlowCoverage.NumElementsCovered / (matchingFlowCoverage.NumElementsCovered + matchingFlowCoverage.NumElementsNotCovered) * 100;
-          }
-        }
-      }
-
+      await FindFlowCoverage(results);
       LintFlowsReport.createOrShow(this.context.extensionUri, results, "Scan");
     } else {
       vscode.window.showInformationMessage('No flow files found.');
---
+++ src/main/libs/FindFlowCoverage.ts
@@ -0,0 +1,22 @@
+import {GetOrgInfo} from "./GetOrgInfo";
+import {GetFlowCoverage} from "./GetFlowCoverage";
+import {GetFlowDefinitionViews} from "./GetFlowDefinitionViews";
+
+export async function FindFlowCoverage(results) {
+
+  const orgInfo = await new GetOrgInfo().getOrgInfo();
+  if(orgInfo.result && orgInfo.result.username){
+    const flowCoverage = await new GetFlowCoverage().getFlowCoverage(orgInfo.result.username);
+    const flowDefinitions = await new GetFlowDefinitionViews().getFlowDefinitionViews(orgInfo.result.username);
+    for (let scanResult of results){
+      const matchingFlowDefinition = flowDefinitions.result.records.find((record) => scanResult.flow.label[0] === record.Label);
+      scanResult['matchingFlowDefinition'] = matchingFlowDefinition;
+      const matchingFlowCoverage = flowCoverage.result.records.find(record => matchingFlowDefinition.ActiveVersionId === record.FlowVersionId);
+      if(matchingFlowCoverage){
+        scanResult['coverage'] = matchingFlowCoverage.NumElementsCovered / (matchingFlowCoverage.NumElementsCovered + matchingFlowCoverage.NumElementsNotCovered) * 100;
+      }
+    }
+  }
+
+
+}
