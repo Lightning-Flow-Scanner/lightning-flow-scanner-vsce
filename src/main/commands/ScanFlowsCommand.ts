9c64a8741ca3415aad73dea0c9f152bb566dff10
Ruben <halmanrf@gmail.com>
2023-08-07 21:49:26 +0200

    Update ScanFlowsCommand.ts
    fix inititial config

--- src/main/commands/ScanFlowsCommand.ts
+++ src/main/commands/ScanFlowsCommand.ts
@@ -32,10 +32,11 @@ export class ScanFlowsCommand extends BaseCommand {
       if((initialConfiguration as unknown as boolean)){
         namingConventionString = await vscode.window.showInputBox({'prompt': 'Readability of a flow is very important. Setting a naming convention for the Flow Name will improve the findability/searchability and overall consistency. You can define your default naming convention using REGEX.', placeHolder: '[A-Za-z0-9]+_[A-Za-z0-9]+', value: '[A-Za-z0-9]+_[A-Za-z0-9]+'});
         apiVersionEvalExpressionString = await vscode.window.showInputBox({'prompt': ' The Api Version has been available as an attribute on the Flow since API v50.0 and it is recommended to limit variation and to update them on a regular basis. Set an expression to set a valid range of API versions(Minimum 50).', placeHolder: '>50', value: '>50'});
-        await vscode.workspace.getConfiguration().update('lightningFlowScanner.namingConvention', namingConventionString, true);
-        await vscode.workspace.getConfiguration().update('lightningFlowScanner.versionAPI', apiVersionEvalExpressionString, true);
         ruleConfig.rules['FlowName'] = {'severity': 'error', 'expression': namingConventionString};
         ruleConfig.rules['APIVersion'] = {'severity': 'error', 'expression': apiVersionEvalExpressionString};
+        await vscode.workspace.getConfiguration().update('lightningFlowScanner.namingConvention', namingConventionString, true);
+        await vscode.workspace.getConfiguration().update('lightningFlowScanner.versionAPI', apiVersionEvalExpressionString, true);
+        await vscode.workspace.getConfiguration().update('lightningFlowScanner.initialConfiguration', false, true);
       } 
       const results: ScanResult[] = core.scan(flows, ruleConfig);
       // todo find coverage asynchronously
