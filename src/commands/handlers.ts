d5e62f260c46348fe891204d6f9e804070c08219
Ruben <halmanrf@gmail.com>
2024-04-07 00:09:46 +0200

    use core parse

--- src/commands/handlers.ts
+++ src/commands/handlers.ts
@@ -109,7 +109,7 @@ export default class Commands {
       ScanOverview.createOrShow(this.context.extensionUri, []);
       const newResults: core.ScanResult[] = core.fix(storedResults);
       for (const newResult of newResults) {
-        await new SaveFlow().execute(newResult.flow, newResult.flow.uri);
+        await new SaveFlow().execute(newResult.flow);
       }
       await CacheProvider.instance.set("results", newResults);
       ScanOverview.createOrShow(this.context.extensionUri, newResults);
--- src/libs/SaveFlow.ts
+++ src/libs/SaveFlow.ts
@@ -1,17 +1,16 @@
 import * as fs from "mz/fs";
 const xml2js = require("xml2js");
-import * as vscode from "vscode";
 import {Flow} from "lightning-flow-scanner-core/out/index";
 
 export class SaveFlow {
-    public async execute(flow: Flow, defaultUri: vscode.Uri) {
-        await this.writeFlow(flow, defaultUri);
-        return defaultUri;
+    public async execute(flow: Flow) {
+        await this.writeFlow(flow);
+        return flow.fsPath;
     }
 
-    private async writeFlow(flow: Flow, pathToWrite: vscode.Uri) {
+    private async writeFlow(flow: Flow) {
        const xml = new xml2js.Builder({rootName: "Flow", xmldec : {'version': '1.0', 'encoding': 'UTF-8'}}).buildObject(flow.xmldata);
-        await fs.writeFile(pathToWrite.fsPath, xml);
+        await fs.writeFile(flow.fsPath, xml);
         return true;
     }
 }
