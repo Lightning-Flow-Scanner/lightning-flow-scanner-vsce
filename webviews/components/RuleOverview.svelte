e8bbd3e8902463b74aa9106d725f2d9d88995f07
junners <jose.junryan@gmail.com>
2025-05-04 20:52:03 -0700

    feat: show beta rules on selection and documentation

--- webviews/components/RuleOverview.svelte
+++ webviews/components/RuleOverview.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
-    import * as core from "lightning-flow-scanner-core";
+    import { getRules, getBetaRules } from "lightning-flow-scanner-core";
     import RuleCard from "./RuleCard.svelte";
 
-    const allRules = core.getRules();
+    const allRules = getRules();
+    const betaRules = getBetaRules();
 </script>
 
 {#if !allRules}
 <p> no rules </p>
 
+{/if}
+
+{#if betaRules}
+    <div id="beta">
+        <h2>Beta Rules</h2>
+        <h3>Modify your .flow-scanner configuration file to take advantage of the beta rules</h3>
+        {#each betaRules as rule, i}
+            <RuleCard>
+                <span slot="label">{i + 1}. {rule.label}</span>
+                <span slot="config"> <a href={rule.uri}>{rule.name}</a></span>
+                <span slot="description">{rule.description}</span>
+                <span slot="docRefs">
+                    <ul>
+                        {#each rule.docRefs as docRef}
+                        <li><a href={docRef.path}>{docRef.label}</a></li>
+                        {/each}
+                    </ul>
+                </span>
+            </RuleCard>
+        {/each}
+    </div>
 {/if}
 {#if allRules}
     <div id="main">
