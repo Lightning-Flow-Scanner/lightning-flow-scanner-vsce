fcf0ceb14e2bfd9d35dfa86d4acc07260103ff05
junners <jose.junryan@gmail.com>
2025-05-18 12:23:47 -0700

    feat: use vscode elements to do a little abstraction

--- beta-webview/App.vue
+++ beta-webview/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import FormDemo from './components/FormDemo.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <FormDemo />
 
 </template>
 
--- beta-webview/main.ts
+++ beta-webview/main.ts
@@ -1,5 +1,19 @@
-import { createApp } from 'vue';
 import './style.css';
+import '@vscode-elements/elements/dist/vscode-button';
+import '@vscode-elements/elements/dist/vscode-checkbox';
+import '@vscode-elements/elements/dist/vscode-form-group';
+import '@vscode-elements/elements/dist/vscode-label';
+import '@vscode-elements/elements/dist/vscode-single-select';
+import '@vscode-elements/elements/dist/vscode-multi-select';
+import '@vscode-elements/elements/dist/vscode-option';
+import '@vscode-elements/elements/dist/vscode-radio';
+import '@vscode-elements/elements/dist/vscode-radio-group';
+import '@vscode-elements/elements/dist/vscode-textfield';
+import { createApp } from 'vue';
 import App from './App.vue';
+
+if (import.meta.env.DEV) {
+  await import('@vscode-elements/webview-playground');
+}
 
 createApp(App).mount('#app');
