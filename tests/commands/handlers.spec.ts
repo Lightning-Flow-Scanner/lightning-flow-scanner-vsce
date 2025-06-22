fe6dfa32700ffbd3c2597fa5c692537ea73a7fdb
junners <jose.junryan@gmail.com>
2025-06-22 01:12:14 -0700

    fix: tests for handlers

--- tests/commands/handlers.spec.ts
+++ tests/commands/handlers.spec.ts
@@ -1,14 +1,17 @@
 import { describe, it, expect, jest } from '@jest/globals';
 import * as cmd from '../../src/commands/handlers';
 
-import { window, ExtensionContext } from 'vscode';
+import * as vsce from 'vscode';
 import * as core from 'lightning-flow-scanner-core';
 import { CacheProvider } from '../../src/providers/cache-provider';
 import { OutputChannel } from '../../src/providers/outputChannel';
+import * as cp from '../../src/providers/config-provider';
 
 jest.mock('lightning-flow-scanner-core');
 jest.mock('../../src/providers/cache-provider');
 jest.mock('../../src/providers/outputChannel');
+jest.mock('../../src/providers/config-provider');
+jest.mock('vscode');
 
 describe('Commands', () => {
   it('should be defined', () => {
@@ -43,7 +46,7 @@ describe('Commands', () => {
         ]),
       } as any;
 
-      const spy = jest.spyOn(window, 'showQuickPick').mockReturnValue([
+      const spy = jest.spyOn(vsce.window, 'showQuickPick').mockReturnValue([
         { label: 'Flow Name', value: 'FlowName' },
         { label: 'API Version', value: 'APIVersion' },
       ] as any);
@@ -57,7 +60,7 @@ describe('Commands', () => {
 
       const extensionContext = jest.fn();
       const command = new cmd.default(
-        extensionContext as unknown as ExtensionContext
+        extensionContext as unknown as vsce.ExtensionContext
       );
 
       await expect(async () => await command['configRules']()).not.toThrow();
@@ -65,11 +68,31 @@ describe('Commands', () => {
 
     it('should read from configuration', async () => {
       const command = new cmd.default({} as any);
+      jest
+        .spyOn(cp.ConfigProvider.prototype, 'discover')
+        .mockImplementation(() =>
+          Promise.resolve({
+            fspath: '/fake/path',
+            config: { rules: { APIName: { severity: 'error' } } },
+          } as any)
+        );
+      (vsce.workspace as any).openTextDocument = jest.fn();
+      (vsce.window as any).showTextDocument = jest.fn();
       await expect(command['ruleConfiguration']()).resolves.not.toThrow();
     });
 
     it('should write to configuration', async () => {
       const command = new cmd.default({} as any);
+      jest
+        .spyOn(cp.ConfigProvider.prototype, 'discover')
+        .mockImplementation(() =>
+          Promise.resolve({
+            fspath: '/fake/path',
+            config: { rules: { APIName: { severity: 'error' } } },
+          } as any)
+        );
+      (vsce.workspace as any).openTextDocument = jest.fn();
+      (vsce.window as any).showTextDocument = jest.fn();
       await expect(command['ruleConfiguration']()).resolves.not.toThrow();
     });
   });
